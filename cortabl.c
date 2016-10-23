2172fd703141605063c66887ad96d8aa291b73ad
darked691 <mehdi.78340@hotmail.fr>
2016-10-23 13:34:29 +0200

    ajout

---
+++ cortabl.c
@@ -0,0 +1,48 @@
+/*
+ * cortab.c
+ * 
+ * Copyright 2016 Utilisateur <user@lubuntu>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#include <stdio.h>
+struct tableau
+{tab[100];
+	int taille;};
+struct Tableau rand_init()
+{struct Tableau T;
+	T.taille=10;
+	int i;
+	for(i=0;i<1;i++)
+	{T.tab[i]=alea(20);}
+	return T;}
+	int alea(int n){ return rand%n;
+		
+		void swap(Tableau *T,int i,intj)
+int temp=t->tab[i];
+T->tab[i]=T->tab[j];
+T->tab[j]=temp;
+}
+int main(int argc, char **argv)
+{srand(time(NULL));
+	struct tableau montab=rand_init();
+	
+	return 0;
+}
+
